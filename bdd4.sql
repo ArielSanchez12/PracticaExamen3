1f74549d33b849e3464c62a7752b40774de02f26
ArielSanchez12 <151105158+ArielSanchez12@users.noreply.github.com>
2025-01-09 18:37:59 -0500

    Add files via upload

---
+++ bdd4.sql
@@ -0,0 +1,11 @@
+create database RegistrosNatacion;
+use RegistrosNatacion;
+
+
+create table UsuariosNatacion(
+id int auto_increment primary key,
+nombre varchar(30),
+edad int
+);
+
+select * from UsuariosNatacion;
